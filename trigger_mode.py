c14d0314ff29c58e7498a8bd196303f271c7e5ba
Kenneth Sim <kensim28@hotmail.com>
2022-09-10 13:47:49 +0800

    bug fix

--- trigger_mode.py
+++ trigger_mode.py
@@ -15,7 +15,6 @@ def upload_file(file_stream, filename, bucketname):
     client = storage.Client()
     bucket = client.bucket(bucketname)
     blob = bucket.blob(filename)
-
+    blob.delete()
     blob.upload_from_filename(file_stream)
     # Ensure the file is publicly readable.
-    blob.make_public()
